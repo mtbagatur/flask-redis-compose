c2c4bb4692d36379fb35c21e4153a9e407cb5f2d
mtbagatur <mt.bagatur@gmail.com>
2025-04-16 12:21:20 +0300

    CI güncelleme

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 import redis
 
+import os
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+r = redis.Redis(host=REDIS_HOST, port=6379)
+
 app = Flask(__name__)
-r = redis.Redis(host="redis", port=6379)
+# r = redis.Redis(host="redis", port=6379)
 
 @app.route("/")
 def index():
