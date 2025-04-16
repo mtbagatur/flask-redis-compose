6300313a3514540d64863ee97cf2be5a4cb36fa4
mtbagatur <mt.bagatur@gmail.com>
2025-04-16 12:26:14 +0300

    Retry sayısı artırıldı, Redis bağlantısı bekletiliyor

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = 6379
 
 # Retry mekanizması (Redis hazır olana kadar bekle)
-for i in range(5):
+for i in range(10):  # 10 deneme yap
     try:
         r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
         r.ping()
         print("Redis bağlantısı başarılı.")
         break
     except redis.exceptions.ConnectionError as e:
-        print(f"Redis bağlantısı kurulamadı, tekrar deneniyor... ({i+1}/5)")
-        time.sleep(2)
+        print(f"Redis bağlantısı kurulamadı, tekrar deneniyor... ({i+1}/10)")
+        time.sleep(3)  # Her denemede 3 saniye bekle
 else:
     raise Exception("Redis'e bağlanılamadı.")
+
 
 @app.route("/")
 def index():
